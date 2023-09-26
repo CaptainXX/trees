add7ad56f464a92c5c2d1db4ed2743446ff969e3
CaptainXX <670909183@qq.com>
2023-09-26 19:50:31 +0800

    1. Add macros for binary tree creations 2. Add base methods for RBTree 3. Add cmake and test codes 4. Refine tree node print 5. Fix node search bug 6. Implemented for left/right rotate

--- binary_tree/bst.hpp
+++ binary_tree/bst.hpp
@@ -2,8 +2,16 @@
 #define BST_HPP
 
 #include <iostream>
+#include <queue>
+#include <vector>
 
 namespace ds_practice {
+
+#define CREATE_BASE_TREETYPE_MEMBERS(TYPE) \
+    T data_; \
+    TYPE *left_; \
+    TYPE *right_; \
+    TYPE *parent_; \
 
 #define CREATE_OPERATORS_FOR_TYPE(TYPE) \
     inline bool operator<(const TYPE& rhs) { return this->data_ < rhs.data_; } \
@@ -13,16 +21,13 @@ namespace ds_practice {
     inline bool operator>(const T& rhs) { return this->data_ > rhs; } \
     inline bool operator==(const T& rhs) { return this->data_ == rhs; } \
     friend std::ostream& operator<<(std::ostream& os, const TYPE& node) { \
-        os << node.data_; \
+        os << node.ToString(); \
         return os; \
     }
 
 template<typename T>
 struct TreeNodeBase {
-    T data_;
-    TreeNodeBase *left_;
-    TreeNodeBase *right_;
-    TreeNodeBase *parent_;
+    CREATE_BASE_TREETYPE_MEMBERS(TreeNodeBase);
 
     TreeNodeBase(): left_(nullptr),
                right_(nullptr),
@@ -32,6 +37,10 @@ struct TreeNodeBase {
                      left_(nullptr),
                      right_(nullptr),
                      parent_(nullptr) {}
+
+    inline std::string ToString() const {
+        return std::to_string(data_);
+    }
 
     CREATE_OPERATORS_FOR_TYPE(TreeNodeBase);
 };
@@ -62,6 +71,7 @@ class BinaryTreeBase {
 
     void Destroy(TreeNode* node);
     void InorderPrint(std::ostream& os, TreeNode* node) const;
+    void TreePrint(std::ostream& os, TreeNode* node) const;
 
     virtual bool InsertRecursively(TreeNode*& parent, TreeNode* node) = 0;
     virtual TreeNode* SearchRecursively(TreeNode* node, const T& target) = 0;
@@ -93,6 +103,7 @@ std::ostream& operator<<(std::ostream& os, const BinaryTreeBase<T, TreeNode>& bs
     os << "[ ";
     bst.InorderPrint(os, bst.root_);
     os << "]\n";
+    bst.TreePrint(os, bst.root_);
     return os;
 }
 
@@ -113,6 +124,53 @@ void BinaryTreeBase<T, TreeNode>::InorderPrint(std::ostream& os, TreeNode* node)
     if (node->right_) InorderPrint(os, node->right_);
 }
 
+template<typename T, typename TreeNode>
+void BinaryTreeBase<T, TreeNode>::TreePrint(std::ostream& os, TreeNode* node) const {
+    std::queue<TreeNode*> node_q;
+    std::vector<std::vector<TreeNode*>> tree;
+
+    node_q.push(node);
+    while (!node_q.empty()) {
+        int n_nodes = node_q.size();
+        bool has_not_null = false;
+
+        tree.push_back({});
+
+        for (int i = 0; i < n_nodes; ++i) {
+            TreeNode* top_node = node_q.front();
+            node_q.pop();
+
+            tree.back().push_back(top_node);
+
+            if (top_node) {
+                node_q.push(top_node->left_);
+                node_q.push(top_node->right_);
+                has_not_null = true;
+            } else {
+                node_q.push(nullptr);
+                node_q.push(nullptr);
+            }
+        }
+
+        if (!has_not_null) {
+            tree.pop_back();
+            break;
+        }
+    }
+
+    int tree_depth = tree.size();
+    for (int i = 0; i < tree_depth; ++i) {
+        for (auto item : tree[i]) {
+            if (item) {
+                os << *item << " ";
+            } else {
+                os << "<x> ";
+            }
+        }
+        os << std::endl;
+    }
+}
+
 // ------------ Binary Search Tree -------------
 
 template<typename T>
@@ -123,6 +181,7 @@ class BinarySearchTree final : public BinaryTreeBase<T> {
 
  protected:
     using TreeNode = typename BinaryTreeBase<T>::TreeNodeType;
+    using BaseTreeType = BinaryTreeBase<T>;
 
     bool InsertRecursively(TreeNode*& parent, TreeNode* node) override;
     TreeNode* SearchRecursively(TreeNode* node, const T& target) override;
@@ -163,9 +222,9 @@ BinarySearchTree<T>::SearchRecursively(TreeNode* node, const T& target) {
     if (!node) return nullptr;
     if (*node == target) return node;
     else if (*node > target) {
-        if (node->left_ && SearchRecursively(node->left_, target)) return node->left_;
+        return SearchRecursively(node->left_, target);
     } else {
-        if (node->right_ && SearchRecursively(node->right_, target)) return node->right_;
+        return SearchRecursively(node->right_, target);
     }
 
     return nullptr;
@@ -186,7 +245,7 @@ bool BinarySearchTree<T>::DeleteRecursively(TreeNode* node, const T& target) {
 
         if (!node->left_ && !node->right_) {
             delete node;
-            if (is_root) BinaryTreeBase<T>::root_ = nullptr;
+            if (is_root) BaseTreeType::root_ = nullptr;
             else {
                 if (is_left_child) parent->left_ = nullptr;
                 else parent->right_ = nullptr;
@@ -195,7 +254,7 @@ bool BinarySearchTree<T>::DeleteRecursively(TreeNode* node, const T& target) {
         } else if (!node->right_) {
             TreeNode *l_node = node->left_;
             if (is_root) {
-                BinaryTreeBase<T>::root_ = l_node;
+                BaseTreeType::root_ = l_node;
             } else {
                 if (is_left_child) parent->left_ = l_node;
                 else parent->right_ = l_node;
@@ -206,7 +265,7 @@ bool BinarySearchTree<T>::DeleteRecursively(TreeNode* node, const T& target) {
         } else if (!node->left_) {
             TreeNode *r_node = node->right_;
             if (is_root) {
-                BinaryTreeBase<T>::root_ = r_node;
+                BaseTreeType::root_ = r_node;
             } else {
                 if (is_left_child) parent->left_ = r_node;
                 else parent->right_ = r_node;
@@ -238,10 +297,8 @@ struct RBTreeNode {
     };
 
     Color color_;
-    T data_;
-    RBTreeNode *left_;
-    RBTreeNode *right_;
-    RBTreeNode *parent_;
+
+    CREATE_BASE_TREETYPE_MEMBERS(RBTreeNode);
 
     RBTreeNode(): color_(Color::Red), data_(),
                   left_(nullptr), right_(nullptr),
@@ -253,6 +310,13 @@ struct RBTreeNode {
                         left_(nullptr), right_(nullptr),
                         parent_(nullptr) {}
 
+    inline bool IsRed() const { return color_ == Color::Red; }
+    inline void SetRed() { color_ = Color::Red; }
+    inline void SetBlack() { color_ = Color::Black; }
+    inline std::string ToString() const {
+        return "(" + std::to_string(data_) + ", " + (color_ == Color::Red ? "Red" : "Black") + ")";
+    }
+
     CREATE_OPERATORS_FOR_TYPE(RBTreeNode);
 };
 
@@ -262,29 +326,196 @@ class RBTree final : public BinaryTreeBase<T, RBTreeNode<T>> {
     RBTree() {}
     ~RBTree() {}
 
+    void TestRotate(bool rot_left, RBTreeNode<T>* node);
+
  protected:
     using TreeNode = RBTreeNode<T>;
+    using BaseTreeType = BinaryTreeBase<T, RBTreeNode<T>>;
 
     bool InsertRecursively(TreeNode*& parent, TreeNode* node) override;
     TreeNode* SearchRecursively(TreeNode* node, const T& target) override;
     bool DeleteRecursively(TreeNode* node, const T& target) override;
 
+    void LeftRotate(TreeNode* node);
+    void RightRotate(TreeNode* node);
+
 };
 
 template<typename T>
+void RBTree<T>::TestRotate(bool rot_left, RBTreeNode<T>* node) {
+    if (rot_left) {
+        LeftRotate(node);
+    } else {
+        RightRotate(node);
+    }
+}
+
+template<typename T>
 bool RBTree<T>::InsertRecursively(TreeNode*& parent, TreeNode* node) {
+    if (!parent) {
+        parent = node;
+        node->SetRed();
+        return true;
+    }
+
+    if (*node < *parent) {
+        if (parent->left_) {
+            return InsertRecursively(parent->left_, node);
+        }
+
+        node->parent_ = parent;
+        parent->left_ = node;
+        return true;
+    } else if (*node > *parent) {
+        if (parent->right_) {
+            return InsertRecursively(parent->right_, node);
+        }
+
+        node->parent_ = parent;
+        parent->right_ = node;
+        return true;
+    }
     return false;
 }
 
 template<typename T>
 typename RBTree<T>::TreeNode*
 RBTree<T>::SearchRecursively(TreeNode* node, const T& target) {
+    if (!node) return nullptr;
+    if (*node == target) return node;
+    else if (*node > target) {
+        return SearchRecursively(node->left_, target);
+    } else {
+        return SearchRecursively(node->right_, target);
+    }
+
     return nullptr;
 }
 
 template<typename T>
 bool RBTree<T>::DeleteRecursively(TreeNode* node, const T& target) {
-    return false;
+    if (!node) return false;
+
+    if (*node > target) {
+        return DeleteRecursively(node->left_, target);
+    } else if (*node < target) {
+        return DeleteRecursively(node->right_, target);
+    } else {
+        TreeNode *parent = node->parent_;
+        bool is_root = (parent == nullptr);
+        bool is_left_child = (!is_root && (parent->left_ == node));
+
+        if (!node->left_ && !node->right_) {
+            delete node;
+            if (is_root) BaseTreeType::root_ = nullptr;
+            else {
+                if (is_left_child) parent->left_ = nullptr;
+                else parent->right_ = nullptr;
+            }
+
+        } else if (!node->right_) {
+            TreeNode *l_node = node->left_;
+            if (is_root) {
+                BaseTreeType::root_ = l_node;
+            } else {
+                if (is_left_child) parent->left_ = l_node;
+                else parent->right_ = l_node;
+            }
+            l_node->parent_ = parent;
+            delete node;
+
+        } else if (!node->left_) {
+            TreeNode *r_node = node->right_;
+            if (is_root) {
+                BaseTreeType::root_ = r_node;
+            } else {
+                if (is_left_child) parent->left_ = r_node;
+                else parent->right_ = r_node;
+            }
+            r_node->parent_ = parent;
+            delete node;
+
+        } else {
+            TreeNode *ino_prev = node->left_;
+            while (ino_prev->right_) {
+                ino_prev = ino_prev->right_;
+            }
+            node->data_ = ino_prev->data_;
+
+            return DeleteRecursively(ino_prev, ino_prev->data_);
+        }
+
+        return true;
+    }
+}
+
+template<typename T>
+void RBTree<T>::LeftRotate(TreeNode* node) {
+    /*      parent               parent
+     *        |                    |
+     *        P                    V
+     *      /   \    ------\     /   \
+     *     F     V   ------/    P     X
+     *         /   \          /   \
+     *        R     X        F     R
+     */
+    TreeNode* v_node = node->right_;
+    TreeNode* r_node = v_node->left_;
+    TreeNode* parent = node->parent_;
+    node->right_ = r_node;
+
+    if (r_node) {
+        r_node->parent_ = node;
+    }
+
+    v_node->parent_ = parent;
+
+    if (!parent) {
+        BaseTreeType::root_ = v_node;
+    } else {
+        if (parent->left_ == node) {
+            parent->left_ = v_node;
+        } else {
+            parent->right_ = v_node;
+        }
+    }
+
+    v_node->left_ = node;
+    node->parent_ = v_node;
+}
+
+template<typename T>
+void RBTree<T>::RightRotate(TreeNode* node) {
+    /*      parent               parent
+     *        |                    |
+     *        P                    F
+     *      /   \    ------\     /   \
+     *     F     V   ------/    D     P
+     *   /   \                      /   \
+     *  D     K                    K     V
+     */
+    TreeNode* f_node = node->left_;
+    TreeNode* k_node = f_node->right_;
+    TreeNode* parent = node->parent_;
+    node->left_ = k_node;
+
+    if (k_node) {
+        k_node->parent_ = node;
+    }
+
+    f_node->parent_ = parent;
+    if (!parent) {
+        BaseTreeType::root_ = f_node;
+    } else {
+        if (parent->left_ == node) {
+            parent->left_ = f_node;
+        } else {
+            parent->right_ = f_node;
+        }
+    }
+
+    f_node->right_ = node;
+    node->parent_ = f_node;
 }
 
 // ------------ AVL Tree -------------
@@ -297,6 +528,7 @@ class AVLTree final : public BinaryTreeBase<T> {
 
  protected:
     using TreeNode = typename BinaryTreeBase<T>::TreeNodeType;
+    using BaseTreeType = BinaryTreeBase<T>;
 
     bool InsertRecursively(TreeNode*& parent, TreeNode* node) override;
     TreeNode* SearchRecursively(TreeNode* node, const T& target) override;
--- binary_tree/test_bst.cc
+++ binary_tree/test_bst.cc
@@ -43,12 +43,58 @@ void TestBinarySearchTree() {
 }
 
 void TestRBTree() {
+    srand(time(nullptr));
     RBTree<int> rbt;
+
+    int n_data = 10;
+    std::vector<int> data_to_ins(n_data);
+    std::vector<int> data = {49, 49, 45, 49, 25, 65, 41, 13, 31, 58};
+    for (int i = 0; i < n_data; ++i) {
+        // data_to_ins[i] = (rand() % (10 * n_data));
+        data_to_ins[i] = data[i];
+    }
+
+    std::cout << "Insert data: ";
+    for (auto x : data_to_ins) {
+        std::cout << x << " ";
+        if (rbt.Insert(x)) {
+            std::cout << "ok, ";
+        }
+    }
+    std::cout << std::endl;
+
+    std::cout << "rbt: " << rbt;
+
+    int rot_test_item = 25;
+    auto node = rbt.Search(rot_test_item);
+    if (node) {
+        std::cout << "Search for " << rot_test_item << ": " << *node << std::endl;
+        rbt.TestRotate(true, node);
+        std::cout << "Tree after left rotate " << rot_test_item << ": " << rbt << std::endl;
+        rbt.TestRotate(false, node);
+        std::cout << "Tree after right rotate " << rot_test_item << ": " << rbt << std::endl;
+    } else {
+        std::cout << "Search " << rot_test_item << " failed\n";
+    }
+
+    // std::cout << "Search exist: " << *rbt.Search(data_to_ins[rand() % data_to_ins.size()]) << std::endl;
+    // std::cout << "Search non-exist: " << (rbt.Search(-1) == nullptr ? "Not found" : "found") << std::endl;
+
+    // for (auto x : data_to_ins) {
+    //     int to_del = x;
+    //     if (rbt.Delete(to_del)) {
+    //         std::cout << "Delete " << to_del << " success\n";
+    //     } else {
+    //         std::cout << "Delete " << to_del << " failed\n";
+    //     }
+    //     std::cout << "bst after delete: " << rbt;
+    // }
 }
 
 } // namespace ds_practice
 
 int main() {
-    ds_practice::TestBinarySearchTree();
+    // ds_practice::TestBinarySearchTree();
+    ds_practice::TestRBTree();
     return 0;
 }
